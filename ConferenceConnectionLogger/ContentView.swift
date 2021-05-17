dcdbba90fdc05003cc5c40f5b42e1d61f6580bd7
mpersic <matej.persic@live.com>
2021-05-17 13:01:33 +0200

    additional changes commit

--- ConferenceConnectionLogger/ContentView.swift
+++ ConferenceConnectionLogger/ContentView.swift
@@ -11,27 +11,50 @@ struct Person: Identifiable, Codable {
     var id = UUID()
     
     enum CodingKeys: CodingKey {
-        case id, image, name
+        case id, name
     }
     var name: String
-    
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
-        // image = try container.decode(Data.self, forKey: .image)
         name = try container.decode(String.self, forKey: .name)
+        id = try container.decode(UUID.self, forKey: .id)
     }
     func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encode(name, forKey: .name)
     }
-    init (image: Image, name: String){
+    init (name: String){
         self.id = UUID()
         self.name = name
     }
+    init (){
+        self.id = UUID()
+        self.name = ""
+    }
+    func getImageUrl() -> URL {
+        let id = self.id.uuidString
+        return getDocumentsDirectory().appendingPathComponent(id)
+    }
 }
 
+//struct DetailView: View {
+//    var person: Person
+//    var image : UIImage = UIImage(named:"afternoon")!
+//    bgImage.frame = CGRect(x: 0, y: 0, width: 100, height: 200)
+//    view.addSubview(bgImage)
+//    var body: some View {
+//        return UIImageView(image: image)
+//    }
+//}
+
+func getDocumentsDirectory() -> URL {
+    let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+    return paths[0]
+}
 
 struct ContentView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @State private var optionalData = UIImage(systemName: "Steve")?.jpegData(compressionQuality: 1)
     @State private var imageSelected = false
     @State private var pickImage = false
     @State private var imageName = ""
@@ -39,15 +62,18 @@ struct ContentView: View {
     @State private var image: Image?
     @State private var showSheet = false
     @State private var inputImage: UIImage?
+    @State private var editName = false
+    @State private var nameImage = false
+    @State private var newPersonName = ""
     var body: some View {
         NavigationView {
             VStack {
                 List(people){ person in
-                    NavigationLink(destination: Image(person.name)){
-                            Image(person.name)
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 44, height: 44)
+                    NavigationLink(destination: Image("Steve")){
+                        Image("Steve")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 32.0, height: 32.0)
                         Text(person.name)
                     }
                 }
@@ -55,12 +81,22 @@ struct ContentView: View {
                     self.showSheet = true
                 }
             }
+            .onAppear(perform: loadUsers)
+            .sheet(isPresented: $nameImage, onDismiss: saveUser){
+                TextField("Enter image name", text: $newPersonName)
+                Button("OK"){
+                    self.presentationMode.wrappedValue.dismiss()
+                }
+            }
         }
         .onAppear(perform: loadUsers)
-        .sheet(isPresented: $showSheet, onDismiss: saveUser) {
+        .sheet(isPresented: $showSheet, onDismiss: imageNamed) {
             ImagePicker(image: self.$inputImage)
         }
-        
+    }
+    
+    func imageNamed(){
+        nameImage = true
     }
     
     func loadImage() {
@@ -78,14 +114,16 @@ struct ContentView: View {
         }
     }
     
+    
     func saveUser(){
+        var newPerson = Person()
         loadImage()
-        let url = getDocumentsDirectory()
+        let id = newPerson.id.uuidString
+        let url = getDocumentsDirectory().appendingPathComponent(id)
         if let jpegData = inputImage?.jpegData(compressionQuality: 0.8) {
             try? jpegData.write(to: url, options: [.atomicWrite, .completeFileProtection])
-            let person = Person(image: image!, name: "Steve")
-            people.append(person)
-            UIImageWriteToSavedPhotosAlbum(inputImage!, nil, nil, nil)
+            newPerson.name = newPersonName
+            people.append(newPerson)
             let encoder = JSONEncoder()
             if let encoded = try? encoder.encode(people) {
                 let defaults = UserDefaults.standard
@@ -93,12 +131,36 @@ struct ContentView: View {
             }
         }
     }
-    
-    func getDocumentsDirectory() -> URL {
-        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        return paths[0]
+}
+
+
+extension UIImageView {
+    func load(url: URL) {
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.image = image
+                    }
+                }
+            }
+        }
     }
 }
+//
+//extension Image {
+//    private var optionalData = UIImage(systemName: "Steve")?.jpegData(compressionQuality: 1)
+//    func load(url: URL){
+//        DispatchQueue.global().async {
+//            if let data = try? Data(contentsOf: url){
+//                if let image = Image(uiImage: UIImage(data: data ?? optionalData!)!) {
+//                    DispatchQueue.main.async {
+//                        self.image = image
+//                    }
+//            }
+//        }
+//    }
+//}
 
 
 struct ContentView_Previews: PreviewProvider {
