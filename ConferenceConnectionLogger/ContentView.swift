3d2a130b0db261823f481ddcb4f031dc2645d41d
mpersic <matej.persic@live.com>
2021-05-17 13:59:06 +0200

    more changes

--- ConferenceConnectionLogger/ContentView.swift
+++ ConferenceConnectionLogger/ContentView.swift
@@ -7,9 +7,8 @@
 
 import SwiftUI
 
-struct Person: Identifiable, Codable {
+struct Person: Identifiable, Codable, Comparable {
     var id = UUID()
-    
     enum CodingKeys: CodingKey {
         case id, name
     }
@@ -31,21 +30,24 @@ struct Person: Identifiable, Codable {
         self.id = UUID()
         self.name = ""
     }
-    func getImageUrl() -> URL {
+    static func < (lhs: Person, rhs: Person) -> Bool {
+        lhs.name < rhs.name
+    }
+    public func getImageUrl() -> URL {
         let id = self.id.uuidString
         return getDocumentsDirectory().appendingPathComponent(id)
     }
 }
 
-//struct DetailView: View {
-//    var person: Person
-//    var image : UIImage = UIImage(named:"afternoon")!
-//    bgImage.frame = CGRect(x: 0, y: 0, width: 100, height: 200)
-//    view.addSubview(bgImage)
-//    var body: some View {
-//        return UIImageView(image: image)
-//    }
-//}
+struct DetailView: View {
+    var optionalData = UIImage(systemName: "Steve")?.jpegData(compressionQuality: 1)
+    var person: Person
+    var body: some View {
+        Image(uiImage: ((loadImageFromURL(person: person)) ?? UIImage(data: optionalData!))!)
+            .resizable()
+            .scaledToFit()
+    }
+}
 
 func getDocumentsDirectory() -> URL {
     let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
@@ -53,14 +55,13 @@ func getDocumentsDirectory() -> URL {
 }
 
 struct ContentView: View {
-    @Environment(\.presentationMode) var presentationMode
-    @State private var optionalData = UIImage(systemName: "Steve")?.jpegData(compressionQuality: 1)
+    private var optionalData = UIImage(systemName: "Steve")?.jpegData(compressionQuality: 1)
     @State private var imageSelected = false
     @State private var pickImage = false
     @State private var imageName = ""
     @State private var people = [Person]()
     @State private var image: Image?
-    @State private var showSheet = false
+    @State private var showImageAddingSheet = false
     @State private var inputImage: UIImage?
     @State private var editName = false
     @State private var nameImage = false
@@ -69,33 +70,34 @@ struct ContentView: View {
         NavigationView {
             VStack {
                 List(people){ person in
-                    NavigationLink(destination: Image("Steve")){
-                        Image("Steve")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 32.0, height: 32.0)
-                        Text(person.name)
+                   NavigationLink(destination: DetailView(person: person)){
+                    Image(uiImage: ((loadImageFromURL(person: person)) ?? UIImage(data: optionalData!))!)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 32.0, height: 32.0)
+                    Text(person.name)
                     }
+                    
                 }
                 Button("Add"){
-                    self.showSheet = true
+                    self.showImageAddingSheet = true
                 }
             }
-            .onAppear(perform: loadUsers)
+            .navigationBarTitle(Text("ConferenceConn"))
             .sheet(isPresented: $nameImage, onDismiss: saveUser){
                 TextField("Enter image name", text: $newPersonName)
                 Button("OK"){
-                    self.presentationMode.wrappedValue.dismiss()
+                    self.nameImage.toggle()
                 }
             }
         }
         .onAppear(perform: loadUsers)
-        .sheet(isPresented: $showSheet, onDismiss: imageNamed) {
+        .sheet(isPresented: $showImageAddingSheet, onDismiss: showImageNamingSheet) {
             ImagePicker(image: self.$inputImage)
         }
     }
     
-    func imageNamed(){
+    func showImageNamingSheet(){
         nameImage = true
     }
     
@@ -107,60 +109,43 @@ struct ContentView: View {
     func loadUsers(){
         let defaults = UserDefaults.standard
         let decoder = JSONDecoder()
-        if let savedPerson = defaults.object(forKey: "SavedPerson") as? Data {
+        if let savedPerson = defaults.object(forKey: "SavedPeople") as? Data {
             if let loadedPerson = try? decoder.decode([Person].self, from: savedPerson) {
                 self.people = loadedPerson
             }
         }
     }
     
-    
     func saveUser(){
         var newPerson = Person()
+        newPerson.name = newPersonName
         loadImage()
-        let id = newPerson.id.uuidString
-        let url = getDocumentsDirectory().appendingPathComponent(id)
+        let url = newPerson.getImageUrl()
         if let jpegData = inputImage?.jpegData(compressionQuality: 0.8) {
             try? jpegData.write(to: url, options: [.atomicWrite, .completeFileProtection])
-            newPerson.name = newPersonName
             people.append(newPerson)
+            people.sort()
             let encoder = JSONEncoder()
             if let encoded = try? encoder.encode(people) {
                 let defaults = UserDefaults.standard
-                defaults.set(encoded, forKey: "SavedPerson")
+                defaults.set(encoded, forKey: "SavedPeople")
             }
         }
     }
 }
 
 
-extension UIImageView {
-    func load(url: URL) {
-        DispatchQueue.global().async { [weak self] in
-            if let data = try? Data(contentsOf: url) {
-                if let image = UIImage(data: data) {
-                    DispatchQueue.main.async {
-                        self?.image = image
-                    }
-                }
-            }
-        }
+
+func loadImageFromURL(person: Person) -> UIImage? {
+    let fileURL = person.getImageUrl()
+    do {
+        let imageData = try Data(contentsOf: fileURL)
+        return UIImage(data: imageData)
+    } catch {
+        print("Error loading image : \(error)")
     }
+    return nil
 }
-//
-//extension Image {
-//    private var optionalData = UIImage(systemName: "Steve")?.jpegData(compressionQuality: 1)
-//    func load(url: URL){
-//        DispatchQueue.global().async {
-//            if let data = try? Data(contentsOf: url){
-//                if let image = Image(uiImage: UIImage(data: data ?? optionalData!)!) {
-//                    DispatchQueue.main.async {
-//                        self.image = image
-//                    }
-//            }
-//        }
-//    }
-//}
 
 
 struct ContentView_Previews: PreviewProvider {
